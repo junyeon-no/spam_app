ac58284832a3adfedc95c894562c66f4a422502f
junyeon <whwns5203@naver.com>
2021-01-05 13:58:54 +0900

    add lstm_ko_model, addtokenizer_ko

--- lstm_test.py
+++ lstm_test.py
@@ -4,27 +4,44 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
-
+import pickle
 
 #https://tykimos.github.io/2017/06/10/Model_Save_Load/ <<<<<참고 블로그
 
-Lstm_model = load_model('.\LSTM_model.h5')
+# Lstm_model = load_mode1l('.\LSTM_model.h5')
+Lstm_model = load_model('.\LSTM_ko_model.h5') #한국어 학습한 모델
+
 max_len = 189
+# d = ["""
+# [Web발신]
+# [긴급재난자금] 상품권이 도착했습니다.
+# 확인해주세요.
+# """]
 d = ["""
 [Web발신]
 [긴급재난자금] 상품권이 도착했습니다.
-확인해주세요.
+확인해주세요. https://bit.ly/3aSTMel
 """]
+# d = ["""
+# SIX chances to win CASH! From 100 to 20,000 pounds txt> CSH11 and send to 87575. Cost 150p/day, 6days, 16+ TsandCs apply Reply HL 4 info
+# """]
 
-### tokenizer loading test
-with open('tokenizer.pickle', 'rb') as handle:
+
+### tokenizer loading test#################################################################
+# with open('tokenizer.pickle', 'rb') as handle:
+#     tokenizer = pickle.load(handle)
+
+with open('tokenizer_ko.pickle', 'rb') as handle: #한국어 학습된 tokenizer
     tokenizer = pickle.load(handle)
-### loading test
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(d)
+### loading test############################################################################
+
+
+# tokenizer = Tokenizer()
+# tokenizer.fit_on_texts(d)
 s = tokenizer.texts_to_sequences(d) # 단어를 숫자값, 인덱스로 변환하여 저장
 print(s) #[ [ 33 , 33, 33] ]
 X_data = s
 data = pad_sequences(X_data, maxlen = max_len)
 print(data)
 print(Lstm_model.predict(data))
+
