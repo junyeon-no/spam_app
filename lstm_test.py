a81b03ac5f0081da8c4b2e7460198f4271ca5574
junyeon <whwns5203@naver.com>
2021-02-10 17:46:18 +0900

    add 형태소분류

--- lstm_test.py
+++ lstm_test.py
@@ -5,11 +5,13 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
 import pickle
+from konlpy.tag import Okt
 
 
 ###########################모델 로드################################
-# Lstm_model = load_mode1l('.\LSTM_model.h5') #영문 스팸 학습 모델
-Lstm_model = load_model('.\LSTM_ko_model.h5') #한글 스팸 학습 모델
+# Lstm_model = load_mode1l('.\model\LSTM_model.h5') #영문 스팸 학습 모델
+# Lstm_model = load_model('.\model\LSTM_ko_model.h5') #한글 스팸 학습 모델
+Lstm_model = load_model('.\model\Spam_Analysis_LSTM_Model.h5') #한글 스팸 학습 모델
 ###########################모델 로드################################
 
 
@@ -38,17 +40,37 @@ d = ["""
 #     tokenizer = pickle.load(handle)
 
 #한글 토큰 객체
-with open('tokenizer_ko.pickle', 'rb') as handle: #한국어 학습된 tokenizer
+with open('Spam_Lstm_tokenizer.pickle', 'rb') as handle: #한국어 학습된 tokenizer
     tokenizer = pickle.load(handle)
 ### loading test############################################################################
 
 
-
-s = tokenizer.texts_to_sequences(d) # 단어를 숫자값, 인덱스로 변환하여 저장
-print(s) 
-X_data = s
-max_len = 189
-data = pad_sequences(X_data, maxlen = max_len) #정수 인코딩된 데이터 패딩
-print(data)
-print(Lstm_model.predict(data))
+#---영문 ---#
+# s = tokenizer.texts_to_sequences(d) # 단어를 숫자값, 인덱스로 변환하여 저장
+# print(s) 
+# X_data = s
+# max_len = 189
+# data = pad_sequences(X_data, maxlen = max_len) #정수 인코딩된 데이터 패딩
+# print(data)
+# print(Lstm_model.predict(data))
+#--영문 ---#
 ####################################
+loaded_model = load_model('.\model\Spam_Analysis_LSTM_Model.h5')
+max_len = 30
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+okt = Okt()
+def sentiment_predict(new_sentence):
+  new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+  score = float(loaded_model.predict(pad_new)) # 예측
+  if(score > 0.5):
+    print("{:.2f}% 확률로 스팸입니다.\n".format(score * 100))
+  else:
+    print("{:.2f}% 확률로 스팸이 아닙니다.\n".format((1 - score) * 100))
+print('hi')
+sentiment_predict('안녕')
+
+
+
